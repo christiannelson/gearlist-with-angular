4142dd7be108a903cdd900f8f1ffda2c459ecf06
Christian Nelson <christian@carbonfive.com>
2016-09-23 15:32:54 -0700

    extract the controller

--- app/components/closet/closet.js
+++ app/components/closet/closet.js
@@ -1,21 +1,26 @@
 (function (angular) {
   'use strict';
 
-  angular.module('myApp').component('closet', {
-    templateUrl: 'components/closet/closet.html',
-    controller: function ClosetController() {
-      this.items = [
-        {
-          name: 'Osprey Exos 38',
-          weight: 34.2
-        }, {
-          name: 'Big Agnes Mystic 15°',
-          weight: 37.0
-        }, {
-          name: 'Thermarest Neoair Xlite',
-          weight: 12.2
-        }
-      ]
-    }
-  });
+  function ClosetController() {
+    this.items = [
+      {
+        name: 'Osprey Exos 38',
+        weight: 34.2
+      }, {
+        name: 'Big Agnes Mystic 15°',
+        weight: 37.0
+      }, {
+        name: 'Thermarest Neoair Xlite',
+        weight: 12.2
+      }
+    ]
+  }
+
+  angular
+    .module('myApp')
+    .component('closet', {
+      templateUrl: 'components/closet/closet.html',
+      controller: ClosetController
+    });
+  
 })(window.angular);
