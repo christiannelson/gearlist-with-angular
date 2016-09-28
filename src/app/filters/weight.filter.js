d735d835f0cceaca1764d1ce8c65423c87d9d738
Christian Nelson <christian@carbonfive.com>
2016-09-28 16:23:51 -0700

    rename common to filters

---
+++ src/app/filters/weight.filter.js
@@ -0,0 +1,16 @@
+(function () {
+
+  function WeightFilter() {
+    return function (value) {
+      var pounds = Math.floor(value / 16.0).toFixed(0);
+      var ounces = (value % 16.0).toFixed(1);
+
+      return pounds + ' lbs ' + ounces + ' oz';
+    };
+  }
+
+  angular
+    .module('gearApp')
+    .filter('weight', WeightFilter);
+
+})();
